3bcba97383c4e1f4fc8b18bf49d28a0294061b79
glorygangaga <gloganaga@tutanota.com>
2025-10-20 22:00:08 +0300

    user-middleware-update

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,4 +1,5 @@
 import { PrismaClient, RoleEnum } from "@prisma/client";
+import { hash } from "argon2";
 
 const prisma = new PrismaClient();
 
@@ -12,6 +13,50 @@ async function main() {
       create: { role_name: roleName },
     })
   }
+
+  const adminRole = await prisma.role.findUnique({ where: { role_name: "admin" } });
+  if (!adminRole) throw new Error("❌ Role 'manager' not found");
+
+
+  const admin = {
+    email: 'admin@gmail.com',
+    password: await hash("123456") ,
+  };
+  console.log(admin);
+
+  await prisma.user.upsert({
+    where: {email: admin.email},
+    update: {},
+    create: {
+      email: admin.email,
+      password: admin.password,
+      created_at: new Date(),
+      updated_at: new Date(),
+      role_id: adminRole.role_id
+    }
+  })
+
+  const managerRole = await prisma.role.findUnique({ where: { role_name: "manager" } });
+  if (!managerRole) throw new Error("❌ Role 'manager' not found");
+
+  const manager = {
+    email: 'manager@gmail.com',
+    password: await hash('123456'),
+  }
+
+  await prisma.user.upsert({
+    where: {email: manager.email},
+    update: {},
+    create: {
+      email: manager.email,
+      password: manager.password,
+      created_at: new Date(),
+      updated_at: new Date(),
+      role_id: managerRole.role_id
+    }
+  });
+
+  console.log("Users created successfully");
 }
 
 main().then(async () => {
--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -49,5 +49,4 @@ export class AuthController {
 
     return true;
   }
-
 }
